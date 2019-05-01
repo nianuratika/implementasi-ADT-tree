65eac027724be6b9618c91d71a5cd9d2efd72f58
nianuratika <48706479+nianuratika@users.noreply.github.com>
2019-05-01 22:49:27 +0700

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,1 +1,15 @@
-
+// Nia Nur atika 1817051015
+/*
+ * C++ Program to Implement Expression Tree
+ */
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>     
+#include <cstring> 
+using namespace std;
+ /** class TreeNode **/
+class TreeNode
+{       
+   
+        TreeNode(char data)
+        {
